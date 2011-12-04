42483c1b578e2e3d40b61e288a33818a98754351
Jochen Skulj <jochen@jochenskulj.de>
2011-12-04 21:14:38 +0100

    HttpHelper added.

--- src/couchhelper.py
+++ src/couchhelper.py
@@ -1,3 +1,6 @@
+
+import httplib
+
 class CouchURI(object):
     """
     Helper class to create a URI
@@ -57,3 +60,103 @@ class CouchURI(object):
         if value:
             newelement = "".join(["?", element, "=", value])
         self._elements.append(newelement)
+
+class HttpHelper(object):
+    """
+    Helper class to support http communication
+    """
+
+    def __init__(self, host, port=5984):
+        """
+        creates an instance
+        - host
+          host to connect
+        - port
+          port to connect
+        """
+        self._host = host
+        self._port = port
+
+    def _connect(self):
+        """
+        connects to the server
+        """
+        return httplib.HTTPConnection(self._host, self._port)
+
+    def get_host(self):
+        """
+        Returns:
+        - host to connect
+        """
+        return self._host
+
+    def get_port(self):
+        """
+        Returns:
+        - port to connect
+        """
+        return self._port
+
+    def get(self, uri):
+        """
+        invokes a get request
+        Parameters:
+        - uri
+          URI to use
+        Returns:
+        - response of the request
+        """
+        c = self.connect()
+        headers = {"Accept": "application/json"}
+        c.request("GET", uri.get_uri_string(), None, headers)
+        return c.getresponse()
+
+    def post(self, uri, body):
+        """
+        invokes a post request
+        Parameters:
+        - uri
+          URI to use
+        - body
+          body to transfer
+        Returns:
+        - response of the request
+        """
+        c = self.connect()
+        headers = {"Content-type": "application/json"}
+        c.request('POST', uri.get_uri_string(), body, headers)
+        return c.getresponse()
+
+    def put(self, uri, body):
+        """
+        invokes a put request
+        Parameters:
+        - uri
+          URI to use
+        - body
+          body to transfer
+        Returns:
+        - response of the request
+        """
+        c = self.connect()
+        if len(body) > 0:
+            headers = {"Content-type": "application/json"}
+            c.request("PUT", uri.get_uri_string(), body, headers)
+        else:
+            c.request("PUT", uri.get_uri_string(), body)
+        return c.getresponse()
+
+    def delete(self, uri):
+        """
+        invokes a post request
+        Parameters:
+        - uri
+          URI to use
+        Returns:
+        - response of the request
+        """
+        c = self.connect()
+        c.request("DELETE", uri.get_uri_string())
+        return c.getresponse()
+
+
